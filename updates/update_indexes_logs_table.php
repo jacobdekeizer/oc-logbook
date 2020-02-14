5c0a746d09bfff74b719babb5ba109507810d4c3
jacobdekeizer <jacobdekeizer@gmail.com>
2020-02-14 21:31:16 +0100

    Update database indexes for faster query results

---
+++ updates/update_indexes_logs_table.php
@@ -0,0 +1,30 @@
+<?php namespace Jacob\Logbook\Updates;
+
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+use October\Rain\Support\Facades\Schema;
+
+class UpdateIndexesLogsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('jacob_logbook_logs', function(Blueprint $table) {
+            $table->dropIndex('jacob_logbook_logs_model_index');
+            $table->dropIndex('jacob_logbook_logs_model_key_index');
+            $table->index([
+                'model',
+                'model_key',
+                'updated_at',
+            ]);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('jacob_logbook_logs', function(Blueprint $table) {
+            $table->dropIndex('jacob_logbook_logs_model_model_key_updated_at_index');
+            $table->index('model');
+            $table->index('model_key');
+        });
+    }
+}
